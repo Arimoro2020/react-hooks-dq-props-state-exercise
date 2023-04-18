6350f63dc4511e7788c5131322b7f5fb570b1aa5
Adeyemi Arimoro <arimoro71@gmail.com>
2023-04-18 00:55:58 -0500

    Add solution

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,37 +1,64 @@
 import React from "react";
-import normalBaby from "../assets/unadulterated-hoglette.png";
+import normalBaby  from "../assets/unadulterated-hoglette.png";
 import SunBaby from "../assets/sun-eyes.png";
 import BlueBaby from "../assets/blue-eyes.png";
 import GlowingBaby from "../assets/glowing-eyes.png";
+// import offspring from "../data";
 
 // is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
+
+
 function BabyHog(props) {
-  function handleChangeWeight(e) {
-    // how can we reuse this for both buttons?
-    // perhaps something with e.target.name === "+"
+  
+  const {eyeColor, allOffSpring,variableWeight, handleChangeWeight} = props
+
+  // const eyeColorMapper = `../assets/${eyeColor}-eyes.png`
+ 
+  let hogPicture;
+
+  switch (true) {
+    case eyeColor==='blue': 
+         hogPicture = BlueBaby;
+           break;
+    case eyeColor ==='glowing': 
+       hogPicture = GlowingBaby;
+         break;
+    case eyeColor ==='sun': 
+       hogPicture = SunBaby;
+         break;
+    default: 
+     hogPicture = normalBaby;
+       break;
   }
+ 
 
-  return (
-    <li className="hogbabies">
-      <h1>Name</h1>
-      <h3>Weight:</h3>
-      <h3>Hobby:</h3>
-      <h4>Eye Color:</h4>
 
-      <button name="+">Increase Weight</button>
-      <button name="-">Decrease Weight</button>
+  return (allOffSpring.map(baby =>
+  <>
+  <li className="hogbabies" key={baby.id}>
+    <h1>Name: {baby.name}</h1>
+    <h3>Weight: {variableWeight} Tons</h3>
+    <h3>Hobby: {baby.hobby}</h3>
+    <h4>Eye Color: {eyeColor}</h4>
 
-      <div className="hb-wrap">
-        <img
-          src={normalBaby}
-          style={{ height: "200px" }}
-          alt="MasterBlasterJrJr"
-        />
-      </div>
-    </li>
-  );
+    <button name="+" onClick={handleChangeWeight}>Increase Weight</button>
+    <button name="-" onClick={handleChangeWeight}>Decrease Weight</button>
+
+    <div className="hb-wrap">
+      <img
+        src={hogPicture}
+        style={{ height: "200px" }}
+        alt="MasterBlasterJrJr"
+      />
+    </div>
+  </li>
+  </>
+
+
+    
+  ));
 }
 
 export default BabyHog;
--- src/components/HogBoss.js
+++ src/components/HogBoss.js
@@ -1,36 +1,43 @@
 import React, { useState } from "react";
 import Boss from "../assets/boss-hog.png";
 import BabyHog from "./BabyHog";
-// import offspring from "../data.js"
+import offspring from "../data.js"
 
 function HogBoss() {
   const [eyeColor, setEyeColor] = useState("blue");
+  const [variableWeight, setVariableWeight] = useState(2.5)
+  // const [allOffSpring, setAllOffSpring] = useState(offspring)
 
-  function handleChangeEyeColor(e) {
-    setEyeColor(e.target.value);
+  function handleChangeWeight(e) {
+    // how can we reuse this for both buttons?
+    // perhaps something with e.target.name === "+"
+    e.target.name === "+"? setVariableWeight(variableWeight=> variableWeight + 1) 
+    : setVariableWeight(variableWeight=> variableWeight - 1)
   }
 
+  
+ 
   return (
     <div>
       <input
         type="radio"
         name="eyeColor"
         value="blue"
-        onChange={handleChangeEyeColor}
+        onChange={(e) =>setEyeColor(eyeColor => e.target.value)}
       />
       Blue<br></br>
       <input
         type="radio"
         name="eyeColor"
         value="sun"
-        onChange={handleChangeEyeColor}
+        onChange={(e) =>setEyeColor(eyeColor => e.target.value)}
       />
       Sun<br></br>
       <input
         type="radio"
         name="eyeColor"
         value="glowing"
-        onChange={handleChangeEyeColor}
+        onChange={(e) =>setEyeColor(eyeColor => e.target.value)}
       />
       Glowing<br></br>
       <h2>Name: Blaster Boss</h2>
@@ -40,9 +47,7 @@ function HogBoss() {
         <img id="boss-blaster" src={Boss} alt="" />
       </div>
       <ul className="hoglist">
-        <BabyHog />
-        <BabyHog />
-        <BabyHog />
+        <BabyHog eyeColor={eyeColor} variableWeight={variableWeight} allOffSpring={offspring} handleChangeWeight={handleChangeWeight}/>
       </ul>
     </div>
   );
